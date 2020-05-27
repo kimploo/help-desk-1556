4a3a9ad0a1de7e7fe3fc1d9fd4d34e65a2326bf9
Hong Shik Branden Kim <kimploo@gmail.com>
2020-05-27 16:56:53 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (event) => {
+    setCount(count + 1);
+    console.log(count);
+    console.log("work");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {count}
+        <button onClick={handleClick}>use this button</button>
       </header>
     </div>
   );
